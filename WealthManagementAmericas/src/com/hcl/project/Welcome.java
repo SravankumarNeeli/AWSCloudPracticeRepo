65658755a16ed7954fe34eb7667c36286a0b0b41
Haritha <Expert@Sravan>
2018-05-02 10:45:12 +0530

    commiting array java program

--- WealthManagementAmericas/src/com/hcl/project/Welcome.java
+++ WealthManagementAmericas/src/com/hcl/project/Welcome.java
@@ -5,6 +5,11 @@ public class Welcome {
 	public static void main(String[] args) {
 		//adding a comment
 		System.out.println("Welcome to java project with Git");
+		String names[]= {"USA","EU","UK"};
+		for(int i=0;i<names.length;i++) {
+			System.out.format("Hello:",names[i]);
+			
+		}
 
 	}
 
