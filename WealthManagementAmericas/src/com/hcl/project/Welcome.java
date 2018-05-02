6675a226d3978f7723c509a537a5282dfef17522
Haritha <Expert@Sravan>
2018-05-02 11:03:12 +0530

    Added a two more countries

--- WealthManagementAmericas/src/com/hcl/project/Welcome.java
+++ WealthManagementAmericas/src/com/hcl/project/Welcome.java
@@ -5,9 +5,10 @@ public class Welcome {
 	public static void main(String[] args) {
 		//adding a comment
 		System.out.println("Welcome to java project with Git");
-		String names[]= {"USA","EU","UK"};
+		String names[]= {"USA","EU","UK","Asia","Australia"};
 		for(int i=0;i<names.length;i++) {
 			System.out.format("Hello:",names[i]);
+			System.out.println("Added 2 more countries");
 			
 		}
 
